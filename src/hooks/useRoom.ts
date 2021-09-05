c2180a8489bdb6081f707ee0bbbf7db2331a347d
WillMS28 <silva.miranda.william@gmail.com>
2021-09-05 19:44:30 -0300

    Like button with add and remove "like"

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { database } from '../services/firebase'
+import { useAuth } from './useAuth'
 
 type firebaseQuestions = Record<string, {
   content: string
@@ -9,6 +10,9 @@ type firebaseQuestions = Record<string, {
   }
   isHighligted: boolean
   isAnswered: boolean
+  likes: Record<string , {
+    authorId: string
+  }>
 }>
 
 type questionType = {
@@ -20,9 +24,12 @@ type questionType = {
   }
   isHighligted: boolean
   isAnswered: boolean
+  likeCount: number
+  likeId: string | undefined
 }
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth()
   const [questions, setQuestions] = useState<questionType[]>([])
   const [title, setTitle] = useState('')
 
@@ -39,14 +46,20 @@ export function useRoom(roomId: string) {
           content: value.content,
           author: value.author,
           isHighligted: value.isHighligted,
-          isAnswered: value.isAnswered
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
         }
       })
 
       setTitle(databaseRoom.title)
       setQuestions(parsedQuestions)
     })
-  }, [roomId])
+
+    return () => {
+      roomRef.off('value')
+    }
+  }, [roomId, user?.id])
 
   return { questions, title }
 }
